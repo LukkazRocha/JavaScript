1e209e48a6196e989e3377809754e1b8118cc5a7
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-10-12 20:05:56 -0300

    realizando exercicio 04

---
+++ one-bit-code/modulo01/aula23/exercicio04/script.js
@@ -0,0 +1,41 @@
+const nome = prompt("Insira seu nome:")
+let resp = prompt("Já visitou alguma cidade? (Sim/Não)")
+let cidade = ""
+let cont = 0
+
+if (resp === "Sim") {
+    cont++
+    cidade = prompt("Qual o nome da cidade?")
+    resp = prompt("Visitou mais alguma cidade?")
+    while (resp === "Sim") {
+        cidade = cidade + ", " + prompt("Qual o nome da cidade?") 
+        cont++
+        resp = prompt("Visitou mais alguma cidade?")
+    }
+    alert(
+        "Cidades que " + nome + " visitou:\n" + cidade +
+        "\nTotal: " + cont + " cidades.")
+} else {
+    alert("Obrigado pela atenção!")
+}
+
+
+/* RESOLUÇÃO DO PROFESSOR:
+const turista = prompt("E aí, turista!, Qual o seu nome?")
+let cidades = ""
+let contagem = 0
+
+let continuar = prompt("Você visitou alguma cidade? (Sim/Não)")
+
+while (continuar === "Sim") {
+    let cidade = prompt("Qual é o nome da cidade visitada?")
+    cidades += " - " + cidade + "\n"
+    contagem++
+    continuar = prompt("Você visitou alguma outra cidade: (Sim/Não)")
+}
+
+alert(
+    "Turista: " + turista +
+    "\nQuantidade de cidades visitadas: " + contagem +
+    "\nCidades visitadas:\n" + cidades
+) */
