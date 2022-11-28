a4be465a076bc6948f9c0fa502b390923af75898
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-11-27 23:29:24 -0300

    Update array-hof.js

--- one-bit-code/modulo01/aula51/array-hof.js
+++ one-bit-code/modulo01/aula51/array-hof.js
@@ -10,3 +10,52 @@ const personagens = [
 
 // Map
 
+/* const nomes = [];
+
+for (let i = 0; i < personagens.length; i++) {
+  nomes.push(personagens[i].nome);
+}
+
+console.log(nomes); */
+
+const nomes = personagens.map(function (personagem) {
+  return personagem.nome;
+});
+
+console.log(nomes);
+
+// Filter
+
+/* const orcs = []
+
+for (let i = 0; i < personagens.length; i++) {
+  if (personagens[i].raca === "Orc") {
+    orcs.push(personagens[i])
+  }
+} */
+
+const orcs = personagens.filter(function (personagem) {
+  return personagem.raca === "Orc";
+});
+
+console.log(orcs);
+
+// Reduce
+
+const nivelTotal = personagens.reduce(function (valorAcumulado, personagem) {
+  return valorAcumulado + personagem.nivel;
+}, 0);
+
+console.log(nivelTotal);
+
+const racas = personagens.reduce(function (valorAcumulado, personagem) {
+  if (valorAcumulado[personagem.raca]) {
+    valorAcumulado[personagem.raca].push(personagem)
+  } else {
+    valorAcumulado[personagem.raca] = [personagem]
+  }
+
+  return valorAcumulado
+}, {})
+
+console.log(racas)
