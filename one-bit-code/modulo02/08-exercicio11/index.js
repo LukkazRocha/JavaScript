e7463ac5d4a86a0c94164aa1d327d39dd3b39287
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-12-07 16:21:33 -0300

    realizando exercicio11

--- one-bit-code/modulo02/08-exercicio11/index.js
+++ one-bit-code/modulo02/08-exercicio11/index.js
@@ -1,15 +1,32 @@
-function adicionar() {
-  const playerList = document.getElementById('players');
+function addPlayer() {
+  const position = document.getElementById("position").value;
+  const name = document.getElementById("name").value;
+  const number = document.getElementById("number").value;
 
-  const ul = document.createElement('ul');
+  const confirmation = confirm(`Escalar ${name} como ${position} ?`);
 
-  const positionLi = document.createElement('li');
-  positionLi.innerText = "Posição do jogador"
-  ul.appendChild(positionLi);
+  if (confirmation) {
+    const teamList = document.getElementById("teamList");
+    const playerItem = document.createElement("li");
+    playerItem.id = `player-${number}`;
+    playerItem.innerText = `${position}: ${name} (${number})`;
+    teamList.appendChild(playerItem);
 
-  playerList.append(ul)
+    document.getElementById("position").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("number").value = "";
+  }
 }
 
-function remover() {
+function removePlayer() {
+  const number = document.getElementById("numberToRemove").value;
+  const playerToRemove = document.getElementById(`player-${number}`);
 
+  const confirmation = confirm(`Remover o jogador ${playerToRemove.innerText}`);
+
+  if (confirmation) {
+    playerToRemove.remove();
+    // document.getElementById("teamList").removeChild(playerToRemove);
+    document.getElementById("numberToRemove").value = "";
+  }
 }
