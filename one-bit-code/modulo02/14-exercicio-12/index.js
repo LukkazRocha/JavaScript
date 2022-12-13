6e184c74c1addea097cae95797fdd519b2f99a41
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2022-12-13 15:39:53 -0300

    praticando alteração de estilos via javascript (dark mode/light mode)

--- one-bit-code/modulo02/14-exercicio-12/index.js
+++ one-bit-code/modulo02/14-exercicio-12/index.js
@@ -34,16 +34,51 @@ addTechBtn.addEventListener("click", function (ev) {
 
   const expLabel = createLabel("Experiência: ");
   const id1 = "expRadio-" + rowIndex + ".1";
-
   const expRadio1 = createInput(
-    "expRadio-" + rowIndex + ".1", // id
-    "0-2 anos", // value
-    "techExp-" + rowIndex, // name
-    "radio" // type
+    id1,
+    "0-2 anos",
+    "techExp-" + rowIndex,
+    "radio"
   );
-  const expLabel1 = createLabel("0-2 anos", "expRadio-" + rowIndex + ".1");
+  const expLabel1 = createLabel("0-2 anos", id1);
+  const id2 = "expRadio-" + rowIndex + ".2";
+  const expRadio2 = createInput(
+    id2,
+    "3-4 anos",
+    "techExp-" + rowIndex,
+    "radio"
+  );
+  const expLabel2 = createLabel("3-4 anos", id2);
+  const id3 = "expRadio-" + rowIndex + ".3";
+  const expRadio3 = createInput(id3, "5+ anos", "techExp-" + rowIndex, "radio");
+  const expLabel3 = createLabel("5+ anos", id3);
 
-  newRow.append(techNameLabel, techNameInput);
+  const removeRowBtn = document.createElement("button");
+  removeRowBtn.type = "button";
+  removeRowBtn.innerText = "Remover";
+  removeRowBtn.addEventListener("click", function () {
+    stackInputs.removeChild(newRow);
+  });
+
+  newRow.append(
+    techNameLabel,
+    techNameInput,
+    expLabel,
+    expRadio1,
+    expLabel1,
+    expRadio2,
+    expLabel2,
+    expRadio3,
+    expLabel3,
+    removeRowBtn
+  );
 
   stackInputs.appendChild(newRow);
+
+  form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+
+    const fullNameInput = document.getElementById("fullName");
+    const inputRow = 
+  })
 });
