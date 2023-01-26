36f4f558315d89e7e6720a981dd25685acba37f1
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-01-26 14:59:13 -0300

    finalizando módulo sobre eventos DOM

--- b7web/modulo03-eventosDOM/exercicio01/index.js
+++ b7web/modulo03-eventosDOM/exercicio01/index.js
@@ -2,7 +2,7 @@ const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 
 function add(e) {
-  if (e.key === 'Enter' && input.value !== '') {
+  if (e.key === 'Enter' && input.value) {
     const newLi = document.createElement('li');
     newLi.innerText = input.value;
     ul.append(newLi);
