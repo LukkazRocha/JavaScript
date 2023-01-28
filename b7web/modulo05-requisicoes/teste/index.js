09eba8bfcfb704dbd690508b57ebe830ac7a5aa8
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-01-28 01:31:32 -0300

    aprendendo sobre async/await e usando na pratica

--- b7web/modulo05-requisicoes/teste/index.js
+++ b7web/modulo05-requisicoes/teste/index.js
@@ -1,21 +1,51 @@
 const url = 'https://jsonplaceholder.typicode.com/posts';
 
-function clicou() {
-  // GET, POST, PUT, DELETE
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      alert(data[0].title);
-    })
-    .catch(() => {
-      alert('DEU RUIM');
-    });
-}
+// async function clicou()
+// const clicou = async () => {
 
-function inserir() {
-  fetch(url, {
+const clicou = async () => {
+  // GET, POST, PUT, DELETE
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    alert(`Título do primeiro post: ${data[0].title}`);
+  } catch (error) {
+    console.log('DEU RUIM, ' + error.message);
+  }
+
+  alert('CLICOU');
+
+  // fetch usando .then
+  /* fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      alert(`Título do primeiro post: ${data[0].title}`);
+    });
+
+  alert('CLICOU'); */
+};
+
+const inserir = async () => {
+  try {
+    let response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: 'Título de teste',
+        body: 'Corpo de teste',
+        userId: 2
+      })
+    });
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log('DEU RUIM, ' + error.message);
+  }
+
+  // fetch usando .then
+  /* fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -26,13 +56,9 @@ function inserir() {
       userId: 2
     })
   })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
-}
+    .then((response) => response.json())
+    .then((data) => console.log(data)); */
+};
 
 document.querySelector('#botao').addEventListener('click', clicou);
 document.querySelector('#inserir').addEventListener('click', inserir);
