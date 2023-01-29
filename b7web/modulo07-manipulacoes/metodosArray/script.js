8441c6454fdf04cfdf04300aad9498c7b8aee6a8
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-01-29 03:20:16 -0300

    se aprofundando ainda mais em manipulação de arrays/objetos como as funções MAP, FILTER, e FIND

---
+++ b7web/modulo07-manipulacoes/metodosArray/script.js
@@ -0,0 +1,29 @@
+let lista = ['Ovo', 'Farinha', 'Massa', 'Laranja'];
+let lista2 = ['Prato', 'Liquidificador', 'Forno'];
+let res;
+
+// res = lista.join(',');
+// res = lista.toString();
+//SAÍDA: Ovo,Farinha,Massa,Laranja
+
+// res = lista.join(' - ');
+//SAÍDA: Ovo - Farinha - Massa - Laranja
+
+// lista.shift();
+// Remove o primeiro elemento da lista
+
+// lista.pop();
+// Remove o último elemento da lista
+
+// res = lista.splice(1, 0, 'Chocolate');
+//SAIDA ['Ovo', 'Chocolate', 'Farinha', 'Massa', 'Laranja']
+
+// lista.sort();
+//SAIDA ['Farinha', 'Laranja', 'Massa', 'Ovo']
+
+// lista.reverse();
+//SAIDA ['Ovo', 'Massa', 'Laranja', 'Farinha']
+
+res = lista;
+
+console.log(res);
